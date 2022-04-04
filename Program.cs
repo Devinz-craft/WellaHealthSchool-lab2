2cc0b66aedeb666b43007f76892b0603aa92dd3a
Devinz-craft <96115866+Devinz-craft@users.noreply.github.com>
2022-04-04 09:04:40 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      //  string Name  =  "Obioha Daniel";
+        //int  = 87;
+        //float  = 0.34;
+        //boolean  = True;
+        // char = #
+        // long = 999
+        // double = 999, 99.9
+        // enum names {John , Harry, Ken, Cynthia}
+        //struct Location
+       // {
+         //   string  city1;
+           // string city2;
+            //}
+dynamic MyDynamicVar = false;
+    Console.WriteLine("Value: {0}, Type: {1}", MyDynamicVar, MyDynamicVar.GetType());
+string  name="Obioha Daniel";
+            Console.WriteLine( name);
+            int a = 87;
+            Console.WriteLine(a);
+        float b = 0.4;
+        Console.WriteLine(b);
+        Boolean c = true;
+        Console.WriteLine(c);
+         // explicit conversion
+         double pi = 3.14;
+         pi = 3.14;
+        int d;
+        d= (int)pi;
+        Console.WriteLine(d);
+         //implicit conversion
+         int number = 128;
+        double bigger = number;
+        
+    }
+}
+            }
+        
+    
+
